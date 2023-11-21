74c2b3ac54f4a012796c55962732678087ae2543
Dlareg <johngeraldcayabyab@gmail.com>
2023-11-21 18:25:12 +0800

    created options table and automatic logging for any model changes

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class BaseModel extends Model
+{
+    use LogsActivity;
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll();
+    }
+}
---
+++ app/Models/Option.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+
+class Option extends BaseModel
+{
+    protected $table = 'options';
+}
