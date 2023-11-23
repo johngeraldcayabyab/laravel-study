395393a5e1b51557b02cd0191eefb5c0ada147c6
Dlareg <johngeraldcayabyab@gmail.com>
2023-11-23 15:30:25 +0800

    Installed tailwind locally instead of a cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>{{config('app.name')}}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-{{--    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>--}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased sans-serif">
 <main>
@@ -14,3 +12,4 @@
 </main>
 </body>
 </html>
+
