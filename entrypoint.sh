d3501238c6477ef20506eace3d293a27b4122909
Dlareg <johngeraldcayabyab@gmail.com>
2023-12-01 19:05:59 +0800

    change strat

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Start PHP-FPM
+# Start PHP-FPM in the background
 php-fpm &
 
-# Wait for PHP-FPM to initialize (adjust sleep duration as needed)
 sleep 5
 
-# Run setup commands
-composer dump-autoload -o
-php artisan key:generate --no-interaction --force
-php artisan migrate:fresh --seed --no-interaction --force
-php artisan optimize:clear
-php artisan storage:link
-php artisan horizon:publish
+# Run additional commands
+docker compose -f docker-compose.prod.yml exec app composer dump-autoload -o
+docker compose -f docker-compose.prod.yml exec app php artisan key:generate --no-interaction --force
+docker compose -f docker-compose.prod.yml exec app php artisan migrate:fresh --seed --no-interaction --force
+docker compose -f docker-compose.prod.yml exec app php artisan optimize:clear
+docker compose -f docker-compose.prod.yml exec app php artisan storage:link
+docker compose -f docker-compose.prod.yml exec app php artisan horizon:publish
 
 # Keep the container running
 tail -f /dev/null
