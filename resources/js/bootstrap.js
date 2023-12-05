ae2ba383d801c70ddc1901294272af1af69f5058
Dlareg <johngeraldcayabyab@gmail.com>
2023-12-05 11:43:57 +0800

    remove bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,35 +9,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import {io} from "socket.io-client";
-
-const socketMain = io("http://0.0.0.0:3003/");
-socketMain.on('connect', () => {
-    console.log('chitae');
-    // socketMain.on('sms-deposit-refresh-browsers', function (payload) {
-    //     window.location.reload();
-    // });
-});
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
